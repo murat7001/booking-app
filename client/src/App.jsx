4eece24abff0456d2318766271e422a99687d930
Murat Yıldırım <2020555068@ogr.cu.edu.tr>
2024-05-08 17:57:23 +0300

    Add RegisterPage component and link to LoginPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import IndexPage from './pages/IndexPage'
 import LoginPage from './pages/LoginPage'
 import Layout from './Layout'
+import RegisterPage from './pages/RegisterPage'
 
 function App() {
 
@@ -12,7 +13,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<IndexPage />} />
-          <Route path='/login' element={<LoginPage />}></Route>
+          <Route path='/login' element={<LoginPage />}/>
+          <Route path='/register' element={<RegisterPage />}/>
         </Route>
       </Routes>
     </>
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 const LoginPage = () => {
     return (
@@ -9,6 +10,7 @@ const LoginPage = () => {
                     <input type="email" placeholder='your@email.com' />
                     <input type="password" placeholder='password' />
                     <button className='primary'>Login</button>
+                    <div className='text-center py-2 text-gray-500'>Don't have an account yet? <Link className='underline text-black' to={"/register"}>Register now</Link> </div>
                 </form>
             </div>
         </div>
