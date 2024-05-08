ff338cba649051c8bd6ebf82ea8a6fc2b70c9d49
Murat Yıldırım <2020555068@ogr.cu.edu.tr>
2024-05-08 19:44:09 +0300

    Add axios for API requests and register user functionality in RegisterPage

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,20 +1,34 @@
 import React, { useState } from 'react'
 import { Link } from "react-router-dom"
+import axios from 'axios'
 
 const RegisterPage = () => {
     const[name,setName] =useState('');
     const [email, setEmail]= useState('');
     const [password, setPassword] = useState('');
 
+    const registerUser = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post('/register', {
+                name,
+                email,
+                password,
+            });
+            alert('Registration succesfully...')
+        } catch (error) {
+            alert('Registration failed...')
+        }
+    }
     return (
         <div className='mt-4 grow flex items-center justify-around'>
             <div className='-mt-64'>
                 <h1 className='text-4xl text-center mb-4'>Register</h1>
-                <form className='max-w-md mx-auto'>
+                <form className='max-w-md mx-auto' onSubmit={registerUser}>
                     <input type="text" placeholder='Your name' value={name} onChange={(e) => {setName(e.target.value)}} />
                     <input type="email" placeholder='your@email.com' value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
-                    <input type="password" placeholder='password' value={password} onChange={(e)=>{e.target.value}}/>
-                    <button className='primary'>Save</button>
+                    <input type="password" placeholder='password' value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+                    <button className='primary'>Register</button>
                     <div className='text-center py-2 text-gray-500'>Already a member? <Link className='underline text-black' to={"/login"}>Login</Link> </div>
                 </form>
             </div>
