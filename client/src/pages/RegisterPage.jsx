9496158fed3285147693852c2356b3df7f0fcb96
Murat Yıldırım <2020555068@ogr.cu.edu.tr>
2024-05-08 18:24:32 +0300

    backend

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom"
 
 const RegisterPage = () => {
+    const[name,setName] =useState('');
+    const [email, setEmail]= useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <div className='mt-4 grow flex items-center justify-around'>
             <div className='-mt-64'>
                 <h1 className='text-4xl text-center mb-4'>Register</h1>
                 <form className='max-w-md mx-auto'>
-                    <input type="text" placeholder='Your name' />
-                    <input type="email" placeholder='your@email.com' />
-                    <input type="password" placeholder='password' />
+                    <input type="text" placeholder='Your name' value={name} onChange={(e) => {setName(e.target.value)}} />
+                    <input type="email" placeholder='your@email.com' value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+                    <input type="password" placeholder='password' value={password} onChange={(e)=>{e.target.value}}/>
                     <button className='primary'>Save</button>
                     <div className='text-center py-2 text-gray-500'>Already a member? <Link className='underline text-black' to={"/login"}>Login</Link> </div>
                 </form>
