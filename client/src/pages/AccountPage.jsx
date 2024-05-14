e9953618cf7bfdf4815a0d8c3d775cf7e8c73b3d
Murat Yıldırım <2020555068@ogr.cu.edu.tr>
2024-05-14 15:45:31 +0300

    "Add loading state and redirect to login for unauthenticated users in AccountPage"

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../UserContext'
+import {Navigate} from 'react-router-dom'
 
 const AccountPage = () => {
+    const {user, ready} = useContext(UserContext);
+    if (!ready) {
+        return 'Loadinggg'
+    }
+
+    if (ready && !user) {
+        return <Navigate to={'/login'}></Navigate>
+    }
     return (
         <div>
-            account page
+            account page for {user.name}
         </div>
     )
 }
