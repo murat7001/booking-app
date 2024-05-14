483a5d75fc6448bcb723b1f4f684352b6e566f26
Murat Yıldırım <2020555068@ogr.cu.edu.tr>
2024-05-14 14:36:54 +0300

    Changed anchor tag to Link component

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -7,12 +7,12 @@ const Header = () => {
     return (
         <div>
             <header className='flex justify-between'>
-                <a href="" className="flex items-center">
+                <Link to={'/'}  className="flex items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-6 -rotate-90">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
                     </svg>
                     <span className='font-bold text-xl'>airbnb</span>
-                </a>
+                </Link>
 
                 <div className='flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300'>
                     <div>Anywhere</div>
